0deeabb513154feccc0d22bec6ad17b2d68aecbd
Arvid Kodehode <arvid.kodehode@gmail.com>
2025-02-03 09:10:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string greeting = GetGreeting();
+        Console.WriteLine(greeting);
+    }
+
+    static string GetGreeting()
+    {
+        int hour = DateTime.Now.Hour;
+        
+        var greetings = new Dictionary<Func<int, bool>, string>
+        {
+            { h => h >= 5 && h < 12, "God morgen!" },
+            { h => h >= 12 && h < 18, "God ettermiddag!" },
+            { h => h >= 18 && h < 22, "God kveld!" },
+            { h => h >= 22 || h < 5, "God natt!" }
+        };
+
+        foreach (var entry in greetings)
+        {
+            if (entry.Key(hour))
+            {
+                return entry.Value;
+            }
+        }
+
+        return "Hei!";
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+Start
+  - Hent nåværende tidspunkt
+  - Bestem hilsen basert på tidspunkt:
+    - 05:00 - 11:59 -> "God morgen!"
+    - 12:00 - 17:59 -> "God ettermiddag!"
+    - 18:00 - 21:59 -> "God kveld!"
+    - 22:00 - 04:59 -> "God natt!"
+  - Returner hilsen til bruker
+Slutt
